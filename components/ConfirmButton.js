b035750bffca8bfe81ea060f37fd16c82ef6e976
Nsstik <nastena-528@mail.ru>
2026-09-05 15:12:07 +0300

    Add ConfirmButton component for confirmation dialog

---
+++ components/ConfirmButton.js
@@ -0,0 +1,17 @@
+'use client';
+
+export function ConfirmButton({ children, className, confirmMessage }) {
+  return (
+    <button
+      type="submit"
+      className={className}
+      onClick={(e) => {
+        if (!window.confirm(confirmMessage)) {
+          e.preventDefault();
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
